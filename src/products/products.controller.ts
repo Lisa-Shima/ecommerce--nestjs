285d0b707c80afe4a36aca9ffd92773973b4d652
Lisa-Shima <shimalisa2@gmail.com>
2025-03-02 19:05:06 +0200

    Committing - Added Role based access control on products CRUDs

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,9 @@
-import { Controller, Get, Post, Body, Put, Delete, Param } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
+import { Controller, Get, Post, Body, Put, Delete, Param, UseGuards } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { RolesGuard } from 'src/auth/guards/roles.guard';
 
 @Controller('products')
 export class ProductsController {
@@ -13,6 +15,7 @@ export class ProductsController {
     }
 
     @Post()
+    @UseGuards(AuthGuard('jwt'),new RolesGuard('admin'))
     async createProduct(@Body() createProduct: CreateProductDto){
         return await this.productsService.createProduct(createProduct)
     }
@@ -23,11 +26,13 @@ export class ProductsController {
     }
 
     @Put(':id')
+    @UseGuards(AuthGuard('jwt'),new RolesGuard('admin'))
     async updateProduct(@Param('id') id: number, @Body() updateProduct: UpdateProductDto){
         return await this.productsService.updateProduct(id, updateProduct)
     }
 
     @Delete(':id')
+    @UseGuards(AuthGuard('jwt'),new RolesGuard('admin'))
     async deleteProduct(@Param('id') id: number){
         return await this.productsService.deleteProduct(id)
     }
